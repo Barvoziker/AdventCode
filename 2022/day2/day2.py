817fd79b77c583a520b67f4ed231a15b9dc00bad
Mathis Buchet <58893042+Barvoziker@users.noreply.github.com>
2022-12-02 12:07:28 +0100

    🖋️ Add comment

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -1,9 +1,17 @@
+## Hand shape : [Win, Draw, Lose]
+## X is Rock
+## Y is Paper
+## Z is Scissors
 a = {
     "X": ["C", "A", "B"],
     "Y": ["A", "B", "C"],
     "Z": ["B", "C", "A"],
 }
 
+## Hand shape : [Win, Draw, Lose]
+## A is Rock
+## B is Paper
+## C is Scissors
 b = {
     "A" : ["Z", "X", "Y"],
     "B" : ["X", "Y", "Z"],
@@ -11,28 +19,30 @@ b = {
 }
 
 
-with open ("input.txt") as f:
-    data = f.read().splitlines()
-    s = 0
+with open ("input.txt") as f: ## Open the file
+    data = f.read().splitlines() ## split on newlines
     
-    for l in data:
-        x, y = l.split(" ")
-        n = a[y].index(x)
-        if n == 0:
+    ## Part 1
+    s = 0 ## init score to 0
+    for l in data: ## for each line
+        x, y = l.split(" ") ## split on space
+        n = a[y].index(x) ## get the index of the hand shape in the dict
+        if n == 0: ## if the index is 0, it's a win
             s += 6
-        if n == 1:
+        if n == 1: ## if the index is 1, it's a draw
             s += 3
-        if y == "X":
+        if y == "X": ## point bonus for X
             s += 1
-        if y == "Y":
+        if y == "Y": ## point bonus for Y
             s += 2
-        if y == "Z":
+        if y == "Z": ## point bonus for Z
             s += 3;
     print(s)
 
-    s = 0
-    for l in data:
-        x, y = l.split(" ")
+    ##Part 2
+    s = 0 ## init score to 0
+    for l in data: ## for each line
+        x, y = l.split(" ") ## split on space
         if y == "X":
             g = 0
         if y == "Y":
